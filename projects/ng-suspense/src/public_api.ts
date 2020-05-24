9b8e99b32a53a9a8e37708387d87c5968bfb0ce5
Yevhen Horbachov <yevhenh@backbase.com>
2020-05-24 12:34:02 +0200

    Install new deps

--- projects/ng-suspense/src/public_api.ts
+++ projects/ng-suspense/src/public_api.ts
@@ -3,3 +3,7 @@
  */
 
 export * from './lib/ng-suspense.module';
+export { SuspenseErrorDirective } from './lib/directives/suspense-error.directive';
+export { SuspensePlaceholderDirective } from './lib/directives/suspense-placeholder.directive';
+export { SuspenseSuccessDirective } from './lib/directives/suspense-success.directive';
+export { SuspenseDirective } from './lib/directives/suspense.directive';
